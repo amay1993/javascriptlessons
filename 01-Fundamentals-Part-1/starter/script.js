901c5c8c17cccfadf1b514aa3938274590b4d7b4
amay1993 <anginnmay@gmail.com>
2021-05-16 11:30:13 +0800

    practice 1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -4,9 +4,16 @@ console.log(40+8+23-10);
 console.log('Jonas');
 console.log(23);
 
-let firstName = 'matilda'
-console.log(firstName)
+let firstName = 'matilda';
+console.log(firstName);
 
-console.log(firstName)
-console.log(firstName)
-console.log(firstName)
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let country = 'Singapore';
+let continent = 'Asia';
+let population = 5640354;
+console.log(country);
+console.log(continent);
+console.log(population);
