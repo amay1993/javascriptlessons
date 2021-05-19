72a0b6ed55644b480be27d7412d0844b3f0aac88
amay1993 <anginnmay@gmail.com>
2021-05-19 08:23:53 +0800

    lect 12

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,19 +1,32 @@
+/*
+my comments
+*/
+/*
 let js = 'amazing';
 console.log(40+8+23-10);
 
 console.log('Jonas');
 console.log(23);
 
-let firstName = 'matilda';
+let firstName = 'Sergio';
 console.log(firstName);
 
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);
-
+*/
+//  Practice 1
 let country = 'Singapore';
 let continent = 'Asia';
 let population = 5640354;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+// type of operator
+true; 
+console.log(true);
+
+console.log(typeof true);
+console.log(typeof country);
+console.log(typeof population);
